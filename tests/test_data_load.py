d6dea147430ccb689f31513147a916c7d6afa6d3
matthewcooper19 <matthew.cooper@nhsx.nhs.uk>
2022-09-13 12:12:05 +0100

    chore: standardise test data input structure

--- tests/test_data_load.py
+++ tests/test_data_load.py
@@ -1,6 +1,5 @@
 from src import data_load
 import pandas as pd
-import pytest
 
 def test_readData():
     dataload = data_load.DataLoader()
@@ -9,16 +8,24 @@ def test_readData():
     assert len(dataload.ward_data) == 5
     assert len(dataload.uni_placements) == 60
 
-
 def test_cohort_generation():
     dataload = data_load.DataLoader()
-    cohort_df = pd.DataFrame([['uni','qual','start']],columns = ['uni_col','qual_col','start_col'])
+    cohort_df = pd.DataFrame(
+        {
+            'uni_col':['uni'],
+            'qual_col':['qual'],
+            'start_col':['start']
+        })
     cohort_df = dataload.createCohort(cohort_df,'student_cohort','uni_col','qual_col','start_col')
     assert cohort_df['student_cohort'][0] == 'uni_qual start'
 
 def test_relative_date_calc():
     dataload = data_load.DataLoader()
-    date_df = pd.DataFrame([['01/01/2020','15/01/2020']],columns = ['start_date','end_date'])
+    date_df = pd.DataFrame(
+        {
+            'start_date':['01/01/2020'],
+            'end_date':['15/01/2020']
+        })
     date_df = dataload.calcRelDateWeeks(date_df,date_df,'weeks_diff','end_date','start_date')
     assert date_df['weeks_diff'][0] == 2
 
