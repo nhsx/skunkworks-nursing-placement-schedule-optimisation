c8708a834c742ad67e06cf226d0c3840f139d64a
matthewcooper19 <matthew.cooper@nhsx.nhs.uk>
2022-09-28 15:41:48 +0100

    test: adding test for get_fitness when no covid compliant wards are available

--- tests/test_schedule.py
+++ tests/test_schedule.py
@@ -89,3 +89,13 @@ def test_capacity_exc_get_fitness():
     sch.get_fitness()
     assert sch.viable == False
     assert sch.non_viable_reason == 'Cap Exceeded'
+
+def test_covid_status_get_fitness():
+    wards = [Ward([0, 'WardA', 'DepartmentB', 4, 'Medium/High', 3, 2, 3, 2]),Ward([1, 'WardB', 'DepartmentA', 1, 'Medium/High', 2, 2, 0, 1])]
+    placements = [Placement([0, 'A_P1,E1', 'CohortA', 2, 1, '2020/01/01', 'P2', "['WardA','WardB']", "['DepA','DepB']", 'Low/Medium'])]
+    slots = [Slot([0,'1']),Slot([1,'2']),Slot([2,'3']),Slot([3,'4'])]
+    sch = Schedule.Schedule(slots = slots, wards = wards, placements = placements, num_weeks = 4)
+    sch.schedule_generation()
+    sch.get_fitness()
+    assert sch.viable == False
+    assert sch.non_viable_reason == 'Covid status not compatible'
