1207f9642cff6d53e67610f25492cfa0b364e798
matthewcooper19 <matthew.cooper@nhsx.nhs.uk>
2022-09-29 14:35:40 +0100

    test: adding test for schedule_quality_check when capacity exceeded

--- tests/test_schedule.py
+++ tests/test_schedule.py
@@ -194,3 +194,14 @@ def test_schedule_quality_check():
     assert incorrect_len_rows == None
     assert cap_exceeded_rows == None
     assert double_booked_rows == None
+
+def test_cap_exc_schedule_quality_check():
+    wards = [Ward([0, 'WardA', 'DepartmentB', 4, 'Low/Medium', 3, 2, 0, 2]),Ward([1, 'WardB', 'DepartmentA', 1, 'Low/Medium', 2, 2, 0, 1])]
+    placements = [Placement([0, 'A_P1,E1', 'CohortA', 2, 1, '2020/01/01', 'P2', "['WardA','WardB']", "['DepA','DepB']", 'Low/Medium'])]
+    slots = [Slot([0,'1']),Slot([1,'2']),Slot([2,'3']),Slot([3,'4'])]
+    sch = Schedule.Schedule(slots = slots, wards = wards, placements = placements, num_weeks = 4)
+
+    sch.schedule_generation()
+    (incorrect_num_plac_rows, incorrect_len_rows, cap_exceeded_rows, double_booked_rows) = sch.schedule_quality_check()
+    cap_exceeded_rows.to_csv('temp.csv')
+    assert len(cap_exceeded_rows) == 2
