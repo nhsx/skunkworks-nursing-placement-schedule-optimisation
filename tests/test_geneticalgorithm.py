d7cb4e6d923d52f9e8c02ff9358ce68eacdaf031
matthewcooper19 <matthew.cooper@nhsx.nhs.uk>
2022-10-04 15:19:14 +0100

    test: adding test for culling

--- tests/test_geneticalgorithm.py
+++ tests/test_geneticalgorithm.py
@@ -171,3 +171,18 @@ def test_generate_offspring():
         assert ga.new_schedules[i]['sched_id'] >= 0
         assert ga.new_schedules[i]['sched_id'] <= 9999
 
+def test_culling():
+    wards = [Ward([0, 'WardA', 'DepartmentB', 4, 'Low/Medium', 3, 2, 3, 2]),Ward([1, 'WardB', 'DepartmentA', 1, 'Low/Medium', 2, 2, 0, 1])]
+    placements = [Placement([0, 'A_P1,E1', 'CohortA', 2, 1, '2020/01/01', 'P2', "['WardA','WardB']", "['DepA','DepB']", 'Low/Medium'])]
+    slots = [Slot([0,'1']),Slot([1,'2']),Slot([2,'3']),Slot([3,'4'])]
+    ga = GeneticAlgorithm(slots, wards, placements, 100, 4)
+    
+    ga.culling(50)
+
+    assert len(ga.new_schedules) > 1
+    for i in range(0,len(ga.new_schedules)):
+        assert ga.new_schedules[i]['fitness'] > 0
+        assert isinstance(ga.new_schedules[i]['schedule'], Schedule) == True
+        assert ga.new_schedules[i]['sched_id'] >= 0
+        assert ga.new_schedules[i]['sched_id'] <= 9999
+
